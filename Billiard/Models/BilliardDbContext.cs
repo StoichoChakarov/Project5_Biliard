d3d843c022d2ddcc2fe9cd79cf361cc3ce6bc5b2
StoichoChakarov <stoicho.chakarov@gmail.com>
2023-02-20 22:58:39 +0200

    Add project files.

---
+++ Billiard/Models/BilliardDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Billiard.Models;
+
+namespace Billiard.Models
+{
+    public class BilliardDbContext:DbContext
+    {
+        public BilliardDbContext(DbContextOptions<BilliardDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BilliardTables> Billiards { get; set; }
+
+        public DbSet<Billiard.Models.Reservations> Reservations { get; set; }
+
+        public DbSet<Billiard.Models.Users> Users { get; set; }
+    }
+}
---
+++ Billiard/Models/BilliardTables.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Billiard.Models
+{
+    public class BilliardTables
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Seats { get; set; }
+        public bool IsForSmokers { get; set; }
+    }
+}
